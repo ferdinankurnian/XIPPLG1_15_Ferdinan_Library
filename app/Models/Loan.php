7235c2eed70822de80e08e8996f66b5968e85090
Ferdinan Kurnia <ferdinankurnian@gmail.com>
2025-02-04 23:08:19 +0700

    Tout Finale

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Loan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'loan_date',
+        'return_date',
+        'status'
+    ];
 }
